0ad265aa6f9745a922b1d688afb262738782f47c
readywebsites <biz499websites@gmsil.com>
2025-05-13 11:22:52 +0530

    changed folder name to static_in_env

--- project/settings.py
+++ project/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # ✅ Point to /static/
-]
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
