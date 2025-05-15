aef8422b4fa0c24af53309be413de62cafb42853
readywebsites <biz499websites@gmsil.com>
2025-05-15 14:18:12 +0530

    .

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,6 @@ from .models import Appointment
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ('fname', 'lname', 'email', 'phone', 'services', 'date', )
+    list_display = ('fname', 'lname', 'email', 'phone', 'services', 'date',)
     search_fields = ('fname', 'lname', 'email')
 
