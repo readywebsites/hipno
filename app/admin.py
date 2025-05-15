a59767b3c732f24e6df9a827645f687b8ca26021
readywebsites <biz499websites@gmsil.com>
2025-05-15 14:13:37 +0530

    trying again for date and time

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,6 @@ from .models import Appointment
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ('fname', 'lname', 'email', 'phone', 'services', 'date')
+    list_display = ('fname', 'lname', 'email', 'phone', 'services', 'date', 'submitted_at')
     search_fields = ('fname', 'lname', 'email')
 
